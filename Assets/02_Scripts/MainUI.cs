8a293cfc98a4ff92c9eafa461ab73c9a37d34b3b
Beahyoeun <bhn0305@gmail.com>
2024-12-16 15:48:46 +0900

    총발사, 재장전 수정

--- Assets/02_Scripts/MainUI.cs
+++ Assets/02_Scripts/MainUI.cs
@@ -26,7 +26,7 @@ public class MainUI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        LeftBulletText.text = $"{gunShooting.LeftCurruntBullet}";
-        RightBulletText.text = $"{gunShooting.RightCurruntBullet}";
+        LeftBulletText.text = $"{gunShooting.leftCurrentBullet}";
+        RightBulletText.text = $"{gunShooting.rightCurrentBullet}";
     }
 }
