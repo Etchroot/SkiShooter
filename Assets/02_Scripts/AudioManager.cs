d8a1343e94727db650bc2310e28c44f5ade5f9bc
Cha MG <jjamyeong@gmail.com>
2025-01-07 10:55:34 +0900

    오디오믹서 효과음추가 테레인수정 FirePoint생성

--- Assets/02_Scripts/AudioManager.cs
+++ Assets/02_Scripts/AudioManager.cs
@@ -11,16 +11,17 @@ public class AudioManager : MonoBehaviour
     [SerializeField] private Slider sfxSlider;
 
     public AudioMixer audioMixer; // 오디오 믹서 연결
-    private string sfxVolumeParameter = "SFXVoulume";
-    private string bgmVolumeParameter = "BGMVoulume";
+    private string sfxVolumeParameter = "SFXVolume";
+    private string bgmVolumeParameter = "BGMVolume";
 
 
 
     void Start()
     {
         // PlayerPrefs에서 저장된 볼륨 값 불러오기
-        float bgmVolume = PlayerPrefs.GetFloat("BGMBVolulme", 0.75f); // 볼륨을 로그 스케일로 표현할 거라서 0.75
+        float bgmVolume = PlayerPrefs.GetFloat("BGMVolume", 0.75f); // 볼륨을 로그 스케일로 표현할 거라서 0.75
         float sfxVolume = PlayerPrefs.GetFloat("SFXVolume", 0.75f);
+        Debug.Log($"초기 저장 사운드 값 : {bgmVolume},{sfxVolume}");
 
         // 슬라이더 초기값 설정
         bgmSlider.value = bgmVolume;
@@ -36,8 +37,21 @@ public class AudioManager : MonoBehaviour
 
     public void SetVolume(float bgmVolume, float sfxVolume)
     {
-        audioMixer.SetFloat(bgmVolumeParameter, Mathf.Log10(bgmVolume) * 20);
-        audioMixer.SetFloat(sfxVolumeParameter, Mathf.Log10(sfxVolume) * 20);
+        if (bgmVolume <= 0.0001f)
+        { audioMixer.SetFloat(bgmVolumeParameter, -80f); }
+        else
+        {
+            audioMixer.SetFloat(bgmVolumeParameter, Mathf.Log10(bgmVolume) * 20);
+        }
+        if (sfxVolume <= 0.0001f)
+        { audioMixer.SetFloat(sfxVolumeParameter, -80f); }
+        else
+        {
+            audioMixer.SetFloat(sfxVolumeParameter, Mathf.Log10(sfxVolume) * 20);
+        }
+
+
+        Debug.Log($"사운드 값 : {bgmVolume},{sfxVolume}");
 
         // PlayerPrefs에 저장
         PlayerPrefs.SetFloat("BGMVolume", bgmVolume);
