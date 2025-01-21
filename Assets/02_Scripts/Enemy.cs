fcdb9526484e18ab6a94dd33542f6c6809910ab6
Cha MG <jjamyeong@gmail.com>
2025-01-21 17:32:15 +0900

    적군의 사격 이펙트와 사운드 추가

--- Assets/02_Scripts/Enemy.cs
+++ Assets/02_Scripts/Enemy.cs
@@ -12,6 +12,14 @@ public class Enemy : MonoBehaviour
     public float rotationSpeed = 10f; // 회전 시간
     private bool isOnCooldown = false; // 공격 쿨다운 여부
     public bool isDead = false; // 적이 죽는 중인지 체크
+    [SerializeField] private AudioClip gnuShotAudio; // 총 발사 소리 클립
+    [SerializeField] private AudioSource audioSource; // 오디오 소스
+    [SerializeField] GameObject gunShotEffectPF1; // 총 발사시 나오는 이펙트 프리펩
+    [SerializeField] GameObject gunShotEffectPF2;
+    [SerializeField] GameObject gunShotEffectPF3;
+    private float effectDuration = 0.1f; // 각 이펙트 지속 시간
+    private float interval = 0.1f; // 각 이펙트 사이 간격
+
 
     private Animator anim;
 
@@ -47,7 +55,9 @@ public class Enemy : MonoBehaviour
         Vector3 direction = (player.position - transform.position).normalized;
         direction.y = 0; // y축 회전을 고정
         Quaternion targetRotation = Quaternion.LookRotation(direction);
-        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
+        // Local 기준 Y축에 추가 각도 적용
+        Quaternion adjustedRotation = targetRotation * Quaternion.Euler(0, 36, 0);
+        transform.rotation = Quaternion.Slerp(transform.rotation, adjustedRotation, Time.deltaTime * rotationSpeed);
     }
 
 
@@ -58,6 +68,9 @@ public class Enemy : MonoBehaviour
         // 대기
         yield return new WaitForSeconds(attackCooldown);
 
+        StartCoroutine(PlayGunEffects());
+        audioSource.PlayOneShot(gnuShotAudio); // 총 소리
+
         Player_New.Instance.TakeDamage();
 
         anim.SetTrigger("SHOOT");
@@ -66,6 +79,23 @@ public class Enemy : MonoBehaviour
         //GameObject projectile = Instantiate(projectilePrefab, firePoint.position, firePoint.rotation);
 
         isOnCooldown = false; // 쿨다운 해제
+    }
+
+    private IEnumerator PlayGunEffects()
+    {
+        GameObject[] effects = { gunShotEffectPF1, gunShotEffectPF2, gunShotEffectPF3 };
+
+        foreach (GameObject effectPrefab in effects)
+        {
+            // 이펙트 생성
+            GameObject effect = Instantiate(effectPrefab, firePoint.position, firePoint.rotation);
+
+            // 일정 시간 후 이펙트 제거
+            Destroy(effect, effectDuration);
+
+            // 다음 이펙트를 위해 대기
+            yield return new WaitForSeconds(interval);
+        }
     }
 
     private void OnDrawGizmosSelected()
