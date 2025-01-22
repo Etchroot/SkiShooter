5205a90855816577fc29ecff4fd3fef0fa2c07e7
Cha MG <jjamyeong@gmail.com>
2025-01-22 10:44:03 +0900

    눈사태 소리 추가 재장전 소리 변경

---
+++ Assets/02_Scripts/AvalancheSoundControl.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.InputSystem.iOS;
+
+public class AvalancheSoundControl : MonoBehaviour
+{
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip audioClip;
+    [SerializeField] private Player_New player_New;
+    [SerializeField] private float minVol = 0.1f;
+    [SerializeField] private float maxVol = 1.0f;
+    [SerializeField] private float minSpeed = 10f;
+    [SerializeField] private float maxSpeed = 40f;
+
+    // void Start()
+    // {
+    //     if (audioSource == null)
+    //     {
+    //         audioSource = gameObject.AddComponent<AudioSource>();
+    //     }
+
+    //     // 오디오 소스 초기화
+    //     audioSource.clip = audioClip;
+    //     audioSource.loop = true;
+    //     audioSource.Play();
+    // }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player_New != null)
+        {
+            // currentSpeed 값에 따라 볼륨 계산
+            float speed = player_New.currentSpeed;
+            float normalizedSpeed = Mathf.Clamp01((speed - minSpeed) / (maxSpeed - minSpeed));
+            audioSource.volume = Mathf.Lerp(maxVol, minVol, normalizedSpeed);
+        }
+    }
+}
